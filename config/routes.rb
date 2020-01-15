74ab3b4e7d6d9c6e3fceff70a9d4454c35338d6c
malvib <malvina.brauge@mmibordeaux.com>
2020-01-15 09:56:22 +0100

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :arts
   resources :themes
-  root to: "themes#index"
+  root to: "arts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
